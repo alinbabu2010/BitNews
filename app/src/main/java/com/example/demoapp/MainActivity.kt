1d8cdc7e1c3bc59ef5dac6c59f50c9d60c03a6f4
Alin Babu <alin@qburst.com>
2020-11-04 12:40:42 +0530

    Add fragment to main activity

--- app/src/main/java/com/example/demoapp/MainActivity.kt
+++ app/src/main/java/com/example/demoapp/MainActivity.kt
@@ -3,9 +3,18 @@ package com.example.demoapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * Main activity class of demo app
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Adding login fragment to activity layout
+        val loginFragment = LoginFragment()
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.fragment_container,loginFragment)
+        fragmentTransaction.commit()
         setContentView(R.layout.activity_main)
     }
 }
