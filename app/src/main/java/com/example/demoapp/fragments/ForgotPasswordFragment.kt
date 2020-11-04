7ef3a0f58bc0309da91674fde1529f44965e5cc3
Alin Babu <alin@qburst.com>
2020-11-04 16:03:52 +0530

    Add functionality to back button

--- app/src/main/java/com/example/demoapp/fragments/ForgotPasswordFragment.kt
+++ app/src/main/java/com/example/demoapp/fragments/ForgotPasswordFragment.kt
@@ -5,10 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.example.demoapp.R
 
 /**
- * A simple [Fragment] subclass for forgot fragment
+ * A simple [Fragment] subclass for forgot password fragment
  */
 class ForgotPasswordFragment : Fragment() {
 
@@ -17,7 +18,12 @@ class ForgotPasswordFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_forgot_password, container, false)
+        val inflatedView = inflater.inflate(R.layout.fragment_forgot_password, container, false)
+
+        inflatedView.findViewById<Button>(R.id.back_button).setOnClickListener {
+            fragmentManager?.popBackStack()
+        }
+        return  inflatedView
     }
 
 }
