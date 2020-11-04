ef46443bba160d2fb4e4b4c978652479f8b4509c
Alin Babu <alin@qburst.com>
2020-11-04 12:20:39 +0530

    Create login fragment and layout

---
+++ app/src/main/java/com/example/demoapp/LoginFragment.kt
@@ -0,0 +1,24 @@
+package com.example.demoapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass for login
+ */
+class LoginFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val inflatedView =  inflater.inflate(R.layout.fragment_login, container, false)
+        return inflatedView
+    }
+
+}
