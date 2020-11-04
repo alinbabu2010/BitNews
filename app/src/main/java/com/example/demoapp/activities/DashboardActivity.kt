c3aedcf27ae7a80e34b729a0689c69df45bc4e6a
Alin Babu <alin@qburst.com>
2020-11-04 16:45:40 +0530

    Add profile fragment to dashboard layout.

--- app/src/main/java/com/example/demoapp/activities/DashboardActivity.kt
+++ app/src/main/java/com/example/demoapp/activities/DashboardActivity.kt
@@ -3,12 +3,15 @@ package com.example.demoapp.activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.demoapp.R
+import com.example.demoapp.fragments.ProfileFragment
 
 class DashboardActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.dashboard_container,ProfileFragment())
+        fragmentTransaction.commit()
         setContentView(R.layout.activity_dashboard)
-
     }
 }
