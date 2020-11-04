ec489f533ef3b0aaffc6dc9f0ddc666665257411
Alin Babu <alin@qburst.com>
2020-11-04 16:07:39 +0530

    Create a dashboard activity

---
+++ app/src/main/java/com/example/demoapp/activities/DashboardActivity.kt
@@ -0,0 +1,14 @@
+package com.example.demoapp.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.demoapp.R
+
+class DashboardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+
+    }
+}
