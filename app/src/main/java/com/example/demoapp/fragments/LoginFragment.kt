e03782dd278ebf24c61032684babeafb4e5a1c51
Alin Babu <alin@qburst.com>
2020-11-04 16:01:11 +0530

    Add functionality to forgot password

--- app/src/main/java/com/example/demoapp/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/demoapp/fragments/LoginFragment.kt
@@ -1,11 +1,20 @@
 package com.example.demoapp.fragments
 
+import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.example.demoapp.R
+
 
 /**
  * A simple [Fragment] subclass for login
@@ -19,7 +28,30 @@ class LoginFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val inflatedView =  inflater.inflate(R.layout.fragment_login, container, false)
+
+        val forgotTextView = inflatedView.findViewById<TextView>(R.id.forgot_password)
+        val ss = SpannableString(forgotTextView.text.toString())
+        val clickableSpan1: ClickableSpan = object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                forgotPassword()
+            }
+
+            override fun updateDrawState(ds: TextPaint) {
+                super.updateDrawState(ds)
+                ds.isUnderlineText = false
+            }
+        }
+        ss.setSpan(clickableSpan1, 0,forgotTextView.text.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        forgotTextView.text = ss
+        forgotTextView.movementMethod = LinkMovementMethod.getInstance()
         return inflatedView
+    }
+
+    private fun forgotPassword() {
+        val fragmentTransaction = fragmentManager?.beginTransaction()
+        fragmentTransaction?.replace(R.id.fragment_container, ForgotPasswordFragment())
+        fragmentTransaction?.addToBackStack(null)
+        fragmentTransaction?.commit()
     }
 
 }
