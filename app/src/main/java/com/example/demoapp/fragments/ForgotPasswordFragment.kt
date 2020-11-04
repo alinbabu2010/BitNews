31892b05022ebe950028d4b0c7fd22511838768c
Alin Babu <alin@qburst.com>
2020-11-04 15:33:31 +0530

    Create a fragment for forgot password.

---
+++ app/src/main/java/com/example/demoapp/fragments/ForgotPasswordFragment.kt
@@ -0,0 +1,23 @@
+package com.example.demoapp.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.demoapp.R
+
+/**
+ * A simple [Fragment] subclass for forgot fragment
+ */
+class ForgotPasswordFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_forgot_password, container, false)
+    }
+
+}
