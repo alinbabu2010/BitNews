d3b535125df6bd923f1c30949d9425beb978bc01
Alin Babu <alin@qburst.com>
2020-11-04 14:01:59 +0530

    Creation of splash activity

---
+++ app/src/main/java/com/example/demoapp/activites/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.example.demoapp.activites
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.demoapp.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
