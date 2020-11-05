b279bc919fc630dd5656e113209ae3faaab6d024
Alin Babu <alin@qburst.com>
2020-11-05 15:05:14 +0530

    Check a user logged in earlier or not at startup

--- app/src/main/java/com/example/demoapp/activities/SplashActivity.kt
+++ app/src/main/java/com/example/demoapp/activities/SplashActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import com.example.demoapp.R
+import com.example.demoapp.utils.sharedPreferenceVariable
 import com.example.demoapp.utils.startNewActivity
 
 /**
@@ -16,10 +17,25 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Handler(Looper.getMainLooper()).postDelayed({
-            startNewActivity(this,MainActivity())
-            this.finish()
-        }, 2000)
+        // Get the values in shared preferences
+        val sharedPreferences = sharedPreferenceVariable(this)
+        val username = sharedPreferences?.getString("username",null)
+        val password = sharedPreferences?.getString("password",null)
+
+        // Check the values in shared preference is null or not
+        if (username.isNullOrBlank() && password.isNullOrBlank()){
+            Handler(Looper.getMainLooper()).postDelayed({
+                startNewActivity(this,MainActivity())
+                this.finish()
+            }, 2000)
+        }else{
+            Handler(Looper.getMainLooper()).postDelayed({
+                startNewActivity(this,DashboardActivity())
+                this.finish()
+            }, 2000)
+        }
+
+
 
     }
 }
