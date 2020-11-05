2667d24d5fa42759ecd5af321f26145a07d781cc
Alin Babu <alin@qburst.com>
2020-11-05 12:09:17 +0530

    Change in splash activity theme

--- app/src/main/java/com/example/demoapp/activities/SplashActivity.kt
+++ app/src/main/java/com/example/demoapp/activities/SplashActivity.kt
@@ -14,7 +14,6 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        supportActionBar?.hide()
 
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, MainActivity::class.java)
