2819449695631f61a60124e42ecb0694f491d436
Alin Babu <alin@qburst.com>
2020-11-06 11:39:46 +0530

    Removing deprecated function

--- app/src/main/java/com/example/demoapp/fragments/ForgotPasswordFragment.kt
+++ app/src/main/java/com/example/demoapp/fragments/ForgotPasswordFragment.kt
@@ -11,7 +11,7 @@ import com.example.demoapp.R
 /**
  * A simple [Fragment] subclass for forgot password fragment
  */
-@Suppress("DEPRECATION")
+
 class ForgotPasswordFragment : Fragment() {
 
     override fun onCreateView(
@@ -22,7 +22,7 @@ class ForgotPasswordFragment : Fragment() {
         val inflatedView = inflater.inflate(R.layout.fragment_forgot_password, container, false)
 
         inflatedView.findViewById<Button>(R.id.back_button).setOnClickListener {
-            fragmentManager?.popBackStack()
+            parentFragmentManager.popBackStack()
         }
         return inflatedView
     }
