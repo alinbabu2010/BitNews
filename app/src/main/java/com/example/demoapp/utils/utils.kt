9bfd6ac9abe7282e308b72479a46f17622fdaac6
Alin Babu <alin@qburst.com>
2020-11-06 11:54:42 +0530

    Creating model package for data class and remove from utils.kt

--- app/src/main/java/com/example/demoapp/utils/utils.kt
+++ app/src/main/java/com/example/demoapp/utils/utils.kt
@@ -1,23 +1,7 @@
 package com.example.demoapp.utils
 
-import android.content.Context
-import android.content.Intent
-import android.content.SharedPreferences
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
-
-
-// Data class for users
-data class Users(val username: String, val password: String, val name: String?, val email: String?)
-
-// Array list of users
-val users: ArrayList<Users> = arrayListOf(
-    Users("alex", "alex123", "Alex John", "alexjohn485@gmail.com"),
-    Users("bob", "bob321", "Bob Thomas", "bobt@outlook.com"),
-    Users("alice1452", "alice1452", "Alice Sanda", "alics8752@gmail.com"),
-    Users("Kevin", "k987321", "Kevin Dapper", "kevind@rediffmail.com")
-)
 
 /**
  * Function to add a new fragment to the layout
