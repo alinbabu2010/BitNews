c9940c8a59dcb14c3f46a9a49456f8789d728bda
Alin Babu <alin@qburst.com>
2020-11-09 10:50:26 +0530

    Adding a logout confirmation alert dialog

--- app/src/main/java/com/example/demoapp/activities/DashboardActivity.kt
+++ app/src/main/java/com/example/demoapp/activities/DashboardActivity.kt
@@ -1,11 +1,16 @@
 package com.example.demoapp.activities
 
 import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.demoapp.R
 import com.example.demoapp.fragments.ProfileFragment
@@ -36,12 +41,29 @@ class DashboardActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return if (item.itemId == R.id.logout_option) {
-            startActivity(Intent(this, MainActivity::class.java))
-            getSharedPreferences("app-userInfo", Context.MODE_PRIVATE).edit().clear().apply()
-            finish()
+            showAlert()
             true
         } else {
             false
         }
+    }
+
+    /*
+     * Function to show alert dialog box after button click.
+     */
+    private fun showAlert(){
+        val builder = AlertDialog.Builder(this,R.style.DialogBoxTheme)
+        builder.setTitle("Logout")
+        builder.setMessage("Do you really want to logout?")
+        builder.setIcon(android.R.drawable.ic_dialog_alert)
+        builder.setPositiveButton("Yes"){ _, _ ->
+            startActivity(Intent(this, MainActivity::class.java))
+            getSharedPreferences("app-userInfo", Context.MODE_PRIVATE).edit().clear().apply()
+            finish()
+        }
+        builder.setNegativeButton("No"){ _: DialogInterface, _: Int -> }
+        val alertDialog: AlertDialog = builder.create()
+        alertDialog.setCancelable(false)
+        alertDialog.show()
     }
 }
