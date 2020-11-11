ffca337728c45e070d4e63ebf3aec4022cb8c0bd
Alin Babu <alin@qburst.com>
2020-11-11 20:42:46 +0530

    Modification in recyclerView adapter and adding views to news item

--- app/src/main/java/com/example/demoapp/adapter/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/demoapp/adapter/RecyclerViewAdapter.kt
@@ -1,15 +1,20 @@
 package com.example.demoapp.adapter
 
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
 import com.example.demoapp.R
 import com.example.demoapp.fragments.NewsFragment
 import com.example.demoapp.models.News
 import com.squareup.picasso.Picasso
+import java.sql.Date
+import java.time.*
+import java.time.format.DateTimeFormatter
 
 /**
  * Adapter class for RecyclerView of [NewsFragment]
@@ -25,6 +30,9 @@ class RecyclerViewAdapter(
         val newsImage: ImageView = view.findViewById(R.id.news_image)
         val newsTitle: TextView = view.findViewById(R.id.news_title)
         val newsDesc: TextView = view.findViewById(R.id.news_desc)
+        val newsSrc : TextView = view.findViewById(R.id.source_textview)
+        val newsAuthor : TextView = view.findViewById(R.id.author_textview)
+        val newsDate : TextView = view.findViewById(R.id.publish_textview)
     }
 
     /**
@@ -42,11 +50,25 @@ class RecyclerViewAdapter(
     /**
      * Replace the contents of a view from news ArrayList (invoked by the layout manager)
      */
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
         val item = news.articles.get(position)
         Picasso.get().load(item.urlToImage).fit().into(holder.newsImage)
         holder.newsTitle.text = item.title
         holder.newsDesc.text = item.description
+
+        val author = "Author: "+item.author
+        holder.newsAuthor.text = author
+
+        val src = "Source: "+item.source.name
+        holder.newsSrc.text = src
+
+        val ldt: LocalDateTime = LocalDateTime.parse(item.publishedAt, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'"))
+        val currentZoneId: ZoneId = ZoneId.systemDefault()
+        val currentZonedDateTime: ZonedDateTime = ldt.atZone(currentZoneId)
+        val format: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+        val publishDate = "Published on "+format.format(currentZonedDateTime)+" at "+ldt.toLocalTime()
+        holder.newsDate.text =  publishDate
     }
 
     /**
