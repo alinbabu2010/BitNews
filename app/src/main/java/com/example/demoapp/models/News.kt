65c4175c6d03982fa162428801a6504b7ef7ecb4
Alin Babu <alin@qburst.com>
2020-11-11 13:56:45 +0530

    Modifying news data model and adding Gson library.

--- app/src/main/java/com/example/demoapp/models/News.kt
+++ app/src/main/java/com/example/demoapp/models/News.kt
@@ -3,4 +3,26 @@ package com.example.demoapp.models
 /**
  * Data class for news
  */
-data class News(val newsImage: String,  val newsTitle: String, val newsDesc : String )
+data class News(val articles:ArrayList<Articles>)
+
+/**
+ * Data class for each news article
+ */
+data class Articles(
+     var source: Source,
+     var author: String,
+     var title: String,
+     var description: String,
+     var url: String,
+     var urlToImage: String,
+     var publishedAt: String,
+     var content: String
+)
+
+/**
+ * Data class for source of news
+ */
+data class Source(
+     var id: Int,
+     var name: String
+)
