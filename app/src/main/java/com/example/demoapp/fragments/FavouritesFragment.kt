212519497a4f26568c41f8f52b4faae834dbc24c
Alin Babu <alin@qburst.com>
2020-11-13 13:56:47 +0530

    Adding viewModel and new layouts to favourite fragment

--- app/src/main/java/com/example/demoapp/fragments/FavouritesFragment.kt
+++ app/src/main/java/com/example/demoapp/fragments/FavouritesFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.demoapp.R
+import com.example.demoapp.viewmodels.FavouritesViewModel
 
 
 /**
@@ -17,6 +19,8 @@ class FavouritesFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        val viewModel = ViewModelProvider(this).get(FavouritesViewModel::class.java)
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_favourites, container, false)
     }
---
+++ app/src/main/java/com/example/demoapp/viewmodels/FavouritesViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.demoapp.viewmodels
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+/**
+ * This class is used to define view model for favourite news storing
+ */
+class FavouritesViewModel : ViewModel() {
+
+
+}
