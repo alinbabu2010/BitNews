08a4a11e6f7ba244cfdf05d8e25cbedbb6a8bf58
Alin Babu <alin@qburst.com>
2020-11-15 12:39:47 +0530

    Added recycler view to favourites and swipe news fragment layout.

--- app/src/main/java/com/example/demoapp/fragments/NewsFragment.kt
+++ app/src/main/java/com/example/demoapp/fragments/NewsFragment.kt
@@ -1,5 +1,6 @@
 package com.example.demoapp.fragments
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.example.demoapp.R
 import com.example.demoapp.adapter.NewsAdapter
 import com.example.demoapp.models.News
@@ -25,26 +27,48 @@ class NewsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        val inflatedView = inflater.inflate(R.layout.fragment_news, container, false)
-        val newsViewModel = activity?.let { ViewModelProvider(it).get(NewsViewModel::class.java) }
+        return inflater.inflate(R.layout.fragment_news, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
         // Call the function to parse JSON file.
         val news = loadJSONFromAsset()
+        val newsViewModel = activity?.let { ViewModelProvider(it).get(NewsViewModel::class.java) }
 
         // Getting recyclerView and invoke layoutManager and recyclerViewAdapter
-        val recyclerView = inflatedView.findViewById<RecyclerView>(R.id.recycler_view)
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view)
         with(recyclerView) {
             layoutManager = LinearLayoutManager(context)
             adapter = newsViewModel?.let {
                 NewsAdapter(news, it) { item ->
                     newsViewModel.newsLiveData.value?.add(item)
-                    println(newsViewModel.newsLiveData.value)
+                    adapter?.notifyDataSetChanged()
                 }
             }
             setHasFixedSize(true)
         }
 
-        return inflatedView
+        // Refresh on swipe by calling recycler view
+        val refreshLayout = view.findViewById<SwipeRefreshLayout>(R.id.swipeRefresh)
+        refreshLayout.setProgressBackgroundColorSchemeColor(Color.YELLOW)
+        refreshLayout.setColorSchemeResources(R.color.secondary_dark)
+        refreshLayout.setOnRefreshListener {
+            with(recyclerView) {
+                newsViewModel?.newsLiveData?.value
+                layoutManager = LinearLayoutManager(context)
+                adapter = newsViewModel?.let {
+                    NewsAdapter(news, it) { item ->
+                        newsViewModel.newsLiveData.value?.add(item)
+                        adapter?.notifyDataSetChanged()
+                    }
+                }
+                setHasFixedSize(true)
+            }
+            refreshLayout.isRefreshing = false
+        }
     }
 
     /**
