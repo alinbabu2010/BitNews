677aa93dfb12ecb9e240926251cb34a37c45d494
Alin Babu <alin@qburst.com>
2020-11-20 15:05:48 +0530

    Modifying view models and added function to view models

--- app/src/main/java/com/example/demoapp/viewmodels/NewsViewModel.kt
+++ app/src/main/java/com/example/demoapp/viewmodels/NewsViewModel.kt
@@ -1,8 +1,11 @@
 package com.example.demoapp.viewmodels
 
+import android.app.Activity
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.demoapp.models.Articles
+import com.example.demoapp.models.News
+import com.google.gson.GsonBuilder
 
 /**
  * This class is used to define view model for favourite news storing
@@ -10,9 +13,54 @@ import com.example.demoapp.models.Articles
 class NewsViewModel : ViewModel() {
 
     var newsLiveData = MutableLiveData<MutableSet<Articles>>()
+    var newsArticles = ArrayList<Articles>()
+    private var articles: MutableSet<Articles>? = mutableSetOf()
 
     init {
         newsLiveData.value = mutableSetOf()
+    }
+
+    /**
+     * Method to get the news from JSON file
+     */
+    fun getNews(activity: Activity): ArrayList<Articles> {
+        val fileData: String? = activity.assets.open("news.json").readBytes().let { String(it) }
+        val gson = GsonBuilder().setPrettyPrinting().create()
+        gson.fromJson(fileData, News::class.java).let { newsArticles = it.articles }
+        return newsArticles
+    }
+
+    /**
+     * Method to add news article to favourites
+     */
+    fun addToFavourites(article: Articles?) {
+        articles = newsLiveData.value
+        article?.let { articles?.add(it) }
+        newsLiveData.postValue(articles)
+    }
+
+    /**
+     * Method to remove news article from favourites
+     */
+    fun removeFromFavourites(article: Articles?) {
+        articles = newsLiveData.value
+        articles?.remove(article)
+        newsLiveData.postValue(articles)
+    }
+
+    /**
+     * Method to check news article in favourites set
+     */
+    fun isFavouriteNews(article: Articles?): Boolean? {
+        return newsLiveData.value?.contains(article)
+    }
+
+    /**
+     * Method to return set of favourites
+     */
+    fun getFavourites(): MutableSet<Articles>? {
+        articles = newsLiveData.value
+        return articles
     }
 
 }
