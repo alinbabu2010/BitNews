c8a88c6498fbce3de190b7e15c4c8943686c9962
Alin Babu <alin@qburst.com>
2020-11-25 15:05:58 +0530

    Adding POST requests for synchronous and asynchronous retrofit

--- app/src/main/java/com/example/demoapp/activities/DummyActivity.kt
+++ app/src/main/java/com/example/demoapp/activities/DummyActivity.kt
@@ -1,12 +1,12 @@
 package com.example.demoapp.activities
 
 import android.os.Bundle
-import android.view.View
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.demoapp.R
 import com.example.demoapp.models.News
+import com.example.demoapp.models.UserPost
+import kotlinx.android.synthetic.main.activity_dummy.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -20,19 +20,31 @@ class DummyActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dummy)
 
+        button_get.setOnClickListener {
+            loadNews()
+        }
+
+        button_post.setOnClickListener {
+            saveData()
+            //thread.start()
+        }
+
     }
 
-    fun loadNews(view: View) {
+
+    /**
+     * Method to make a GET request using retrofit
+     */
+    private fun loadNews() {
 
         // Retrofit builder
-        val retrofit = Retrofit.Builder()
+        val retrofit: Retrofit = Retrofit.Builder()
             .baseUrl("http://newsapi.org/v2/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
         // Object to call methods
-        val newsAPI = retrofit.create(NewsAPI::class.java)
-
+        val newsAPI: NewsAPI = retrofit.create(NewsAPI::class.java)
         val call: Call<News> = newsAPI.getNews()
         call.enqueue(object : Callback<News> {
             override fun onResponse(call: Call<News>, response: Response<News>) {
@@ -55,13 +67,64 @@ class DummyActivity : AppCompatActivity() {
                     }
                 }
 
-                view.findViewById<TextView>(R.id.textView_out).text = stringBuilder
+                textView_out.text = stringBuilder
             }
 
             override fun onFailure(call: Call<News>, t: Throwable) {
                 Toast.makeText(baseContext, t.message.toString(), Toast.LENGTH_SHORT).show()
             }
         })
+    }
+
+    /**
+     * Method to make a POST request using retrofit
+     */
+    private fun saveData() {
+
+        // Retrofit builder
+        val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl("https://jsonplaceholder.typicode.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        // Object to call methods
+        val newsAPI: NewsAPI = retrofit.create(NewsAPI::class.java)
+        val newPost = UserPost(1, 2, "Sample Title", "Hello my dear friend!")
+        val callPost: Call<UserPost> = newsAPI.setPost(newPost)
+
+        // Asynchronous method
+        callPost.enqueue(object : Callback<UserPost> {
+            override fun onResponse(call: Call<UserPost>, response: Response<UserPost>) {
+                val message = " ${response.code()} \n ${response.raw()} \n" + " ${response.body()}"
+                textView_out.text = message
+            }
+
+            override fun onFailure(call: Call<UserPost>, t: Throwable) {
+                textView_out.text = t.message.toString()
+            }
+        })
+
+
+    }
+
+
+    // Thread to execute Synchronous method of retrofit POST request
+    private val thread = Thread() {
+
+        // Retrofit builder
+        val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl("https://jsonplaceholder.typicode.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        // Object to call methods
+        val newsAPI: NewsAPI = retrofit.create(NewsAPI::class.java)
+        val newPost = UserPost(1, 2, "Sample Title", "Hello my dear friend!")
+        val callPost: Call<UserPost> = newsAPI.setPost(newPost)
+
+        val post = callPost.execute()
+        val message = " ${post.code()} \n ${post.message()}"
+        runOnUiThread{ textView_out.text = message }
     }
 
 }
--- app/src/main/java/com/example/demoapp/activities/NewsAPI.kt
+++ app/src/main/java/com/example/demoapp/activities/NewsAPI.kt
@@ -1,8 +1,11 @@
 package com.example.demoapp.activities
 
 import com.example.demoapp.models.News
+import com.example.demoapp.models.UserPost
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface NewsAPI {
 
@@ -10,5 +13,10 @@ interface NewsAPI {
 
     @GET("top-headlines?sources=techcrunch&apiKey=19b71eb781b0404b93feafc1badf4324")
     fun getNews() : Call<News>
+
+    //https://jsonplaceholder.typicode.com/   posts
+
+    @POST("posts")
+    fun setPost( @Body userPost: UserPost) : Call<UserPost>
 
 }
