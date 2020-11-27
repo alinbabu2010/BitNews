725f26b38d8c2d66465ad2360b8d49a0e2b40706
Alin Babu <alin@qburst.com>
2020-11-27 11:04:17 +0530

    Renaming class Services to RetrofitService

--- app/src/main/java/com/example/demoapp/utils/RetrofitService.kt
+++ app/src/main/java/com/example/demoapp/utils/RetrofitService.kt
@@ -1,14 +1,14 @@
 package com.example.demoapp.utils
 
-import com.example.demoapp.api.PostsAPI
+import com.example.demoapp.api.UserPostAPI
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class Services {
+class RetrofitService {
 
-    fun getService(baseURL : String): PostsAPI {
+    fun getService(baseURL : String): UserPostAPI {
         val retrofit = getRetrofit(baseURL)
-        return retrofit.create(PostsAPI::class.java)
+        return retrofit.create(UserPostAPI::class.java)
     }
 
     fun getRetrofit(baseURL: String): Retrofit {
