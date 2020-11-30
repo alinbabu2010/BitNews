25c3013ba57bc47aee0d5f93f7a8672750cc374d
Alin Babu <alin@qburst.com>
2020-11-30 14:28:45 +0530

    Removing user array list

--- app/src/main/java/com/example/demoapp/models/Users.kt
+++ app/src/main/java/com/example/demoapp/models/Users.kt
@@ -6,12 +6,3 @@ package com.example.demoapp.models
 
 data class Users(val username: String, val password: String, val name: String?, val email: String?)
 
-/**
- * Array list of users
- */
-val users: ArrayList<Users> = arrayListOf(
-    Users("alex", "alex123", "Alex John", "alexjohn485@gmail.com"),
-    Users("bob", "bob321", "Bob Thomas", "bobt@outlook.com"),
-    Users("alice1452", "alice1452", "Alice Sanda", "alics8752@gmail.com"),
-    Users("Kevin", "k987321", "Kevin Dapper", "kevind@rediffmail.com")
-)
