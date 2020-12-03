467097d703d54c4fb545d6b4f1ccbd64c8f06d11
Alin Babu <alin@qburst.com>
2020-12-03 19:48:53 +0530

    Modifying constants

--- app/src/main/java/com/example/demoapp/utils/Const.kt
+++ app/src/main/java/com/example/demoapp/utils/Const.kt
@@ -1,14 +1,16 @@
 package com.example.demoapp.utils
 
-const val FIELD_EMPTY_MESSAGE = "Fields cannot be empty"
-const val WRONG_CREDENTIALS_MESSAGE = "Incorrect credentials entered"
-const val EMPTY_FILTER_MESSAGE = "Select at least one filter"
-const val PASSWORD_RESET_MESSAGE = "Password reset link send to email"
-const val INVALID_EMAIL_MESSAGE = "Invalid email address entered"
-const val ALERT_DIALOG_QUESTION = "Do you really want to logout?"
-const val LOGOUT_STRING = "Logout"
-const val YES = "Yes"
-const val NO = "No"
-const val NETWORK_ERROR_MESSAGE = "Network failure"
-const val SERVER_ERROR = "Sorry, Server error occurred"
-const val NOT_FOUND = "Item not found"
+/**
+ * Class for holding constant values
+ */
+class Const {
+
+    companion object {
+        const val ARTICLE = "article"
+        const val SHARE_TYPE = "text/plain"
+        const val API_KEY = "19b71eb781b0404b93feafc1badf4324"
+        const val NAME_STRING = "name"
+        const val USERNAME_STRING = "username"
+        const val EMAIL_STRING = "email"
+    }
+}
