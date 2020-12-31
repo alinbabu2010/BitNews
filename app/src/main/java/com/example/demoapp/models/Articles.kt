6193961f058af5c63d841b42e842cdeaa41dbf1a
Alin Babu <alin@qburst.com>
2020-12-31 17:44:24 +0530

    Modifying primary keys

--- app/src/main/java/com/example/demoapp/models/Articles.kt
+++ app/src/main/java/com/example/demoapp/models/Articles.kt
@@ -3,20 +3,17 @@ package com.example.demoapp.models
 import android.os.Parcelable
 import androidx.room.Embedded
 import androidx.room.Entity
-import androidx.room.Index
-import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
 
 /**
  * Data class for each news article
  */
-@Entity(indices = [Index(value = ["title"], unique = true)]) @Parcelize
+@Entity(primaryKeys=["title","author"]) @Parcelize
 data class Articles(
-    @PrimaryKey(autoGenerate = true) val id : Int = 0,
     @Embedded @SerializedName("source") val source: Source? = Source(),
-    @SerializedName("author") val author: String? = "",
-    @SerializedName("title") val title: String? = "",
+    @SerializedName("author") val author: String = "",
+    @SerializedName("title") val title: String = "",
     @SerializedName("description") val description: String? = "",
     @SerializedName("url") val url: String? = "",
     @SerializedName("urlToImage") val imageUrl: String? = "",
