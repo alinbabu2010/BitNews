93562c463df17f744414652ad352492c702b3c3a
Alin Babu <alin@qburst.com>
2020-12-31 17:45:33 +0530

    Modifying article repository

--- app/src/main/java/com/example/demoapp/repository/ArticleRepository.kt
+++ app/src/main/java/com/example/demoapp/repository/ArticleRepository.kt
@@ -8,33 +8,27 @@ import com.example.demoapp.api.RetrofitManager
 import com.example.demoapp.database.ArticlesDAO
 import com.example.demoapp.models.Articles
 import com.example.demoapp.models.News
-import com.example.demoapp.ui.fragments.dashboard.NewsFragment
-import com.example.demoapp.utils.Utils.Companion.checkNetworkConnection
 
 /**
  * Repository class for handling news articles
  */
-class ArticleRepository(private val  articlesDAO: ArticlesDAO) {
+class ArticleRepository(private val articlesDAO: ArticlesDAO) {
 
     val newsLiveData = MutableLiveData<Resource<News?>>()
-    val readAllArticles : LiveData<List<Articles>> = articlesDAO.getAllArticles()
+
+    fun readArticles(): LiveData<List<Articles>> = articlesDAO.getAllArticles()
 
     fun getArticles() {
-        val articles = readAllArticles
-        if (articles.value == null) {
-            checkNetworkConnection(NewsFragment.context) {
-                RetrofitManager.getRetrofitService {
-                    when (it) {
-                        is APIResponse.Success -> newsLiveData.postValue(Resource.success(it.data))
-                        is APIResponse.Error -> newsLiveData.postValue(Resource.error(it.error))
-                        is APIResponse.Failure -> newsLiveData.postValue(Resource.failure(it.exception))
-                    }
+        RetrofitManager.getRetrofitService {
+                when (it) {
+                    is APIResponse.Success -> newsLiveData.postValue(Resource.success(it.data))
+                    is APIResponse.Error -> newsLiveData.postValue(Resource.error(it.error))
+                    is APIResponse.Failure -> newsLiveData.postValue(Resource.failure(it.exception))
                 }
-            }
         }
     }
 
-    suspend fun addArticles(articles: Articles){
+    suspend fun addArticles(articles: Articles) {
         articlesDAO.addArticle(articles)
     }
 }
