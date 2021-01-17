baf1d30e8ed5d94864eaf033a04c668b27b47a4b
Alin Babu <alin@qburst.com>
2021-01-17 22:28:19 +0530

    Adding an new fragment for listing users for chat

---
+++ app/src/main/java/com/example/demoapp/ui/fragments/dashboard/ChatUsersFragment.kt
@@ -0,0 +1,70 @@
+package com.example.demoapp.ui.fragments.dashboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.demoapp.adapter.ChatUsersAdapter
+import com.example.demoapp.databinding.FragmentChatUsersBinding
+import com.example.demoapp.viewmodels.AccountsViewModel
+
+/**
+ * A simple [Fragment] subclass used to display to list of users or chatting
+ */
+class ChatUsersFragment : Fragment() {
+
+    private lateinit var binding : FragmentChatUsersBinding
+    private var adapter : ChatUsersAdapter? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentChatUsersBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val layoutManager = LinearLayoutManager(context)
+        val accountsViewModel = ViewModelProviders.of(this).get(AccountsViewModel::class.java)
+        accountsViewModel.getUserList {
+            adapter = ChatUsersAdapter(it,activity)
+        }
+        val chatMessageCount = adapter?.itemCount as Int
+        adapter?.registerAdapterDataObserver(object : RecyclerView.AdapterDataObserver() {
+            override fun onItemRangeInserted(positionStart: Int, itemCount: Int) {
+                super.onItemRangeInserted(positionStart, itemCount)
+                val lastVisiblePosition =
+                    layoutManager.findLastCompletelyVisibleItemPosition()
+                // If the recycler view is initially being loaded or the
+                // user is at the bottom of the list, scroll to the bottom
+                // of the list to show the newly added message.
+                if (lastVisiblePosition == -1 ||
+                    positionStart >= chatMessageCount - 1 &&
+                    lastVisiblePosition == positionStart - 1
+                ) {
+                    binding.recyclerviewChatUsers.smoothScrollToPosition(positionStart)
+                }
+            }
+        })
+        binding.recyclerviewChatUsers.layoutManager = layoutManager
+        binding.recyclerviewChatUsers.adapter = adapter
+
+    }
+
+    override fun onPause() {
+        adapter?.stopListening()
+        super.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        adapter?.startListening()
+    }
+
+}
