2d4f34d24dbc23b8c1ded8f159918086b7a124a8
Alin Babu <alin@qburst.com>
2021-01-17 22:31:00 +0530

    Adding activity for full screen image

---
+++ app/src/main/java/com/example/demoapp/ui/activities/dashboard/FullscreenImageActivity.kt
@@ -0,0 +1,43 @@
+package com.example.demoapp.ui.activities.dashboard
+
+import android.os.Bundle
+import android.transition.Fade
+import android.view.MenuItem
+import android.view.Window
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.example.demoapp.R
+
+/**
+ * A full-screen activity that shows the full size image
+ */
+class FullscreenImageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        with(window) {
+            requestFeature(Window.FEATURE_ACTIVITY_TRANSITIONS)
+            sharedElementExitTransition = Fade()
+        }
+        setContentView(R.layout.activity_fullscreen_image)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        val imageUrl = intent.getStringExtra("image")
+        val messageImageView : ImageView = findViewById(R.id.full_imageView)
+        Glide.with(this).load(imageUrl).into(messageImageView)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        supportFinishAfterTransition()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (R.id.home == item.itemId) {
+            onBackPressed()
+            true
+        } else super.onOptionsItemSelected(item)
+    }
+
+}
